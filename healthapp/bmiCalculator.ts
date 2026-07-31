7c45d5066c9c5cc79d7eb10a6b5e87886d400a08
niinaIsHere <niina.kampman@gmail.com>
2026-07-31 22:13:05 +0300

    Ch3 ex 1

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,21 @@
+const calculateBmi = (height: number, mass: number) => {
+  const height_in_meters = height / 100;
+
+  const bmi = mass / height_in_meters ** 2;
+
+  let message = "";
+  if (bmi < 18.5) {
+    message = "Underweight";
+  } else if (bmi <= 25 && bmi >= 18.5) {
+    message = "Normal range";
+  } else if (bmi > 25) {
+    message = "Overweight";
+  }
+
+  console.log(message);
+};
+
+const mass: number = Number(process.argv[2]);
+const height: number = Number(process.argv[3]);
+
+calculateBmi(mass, height);
