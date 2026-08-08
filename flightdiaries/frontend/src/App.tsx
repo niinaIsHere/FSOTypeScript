966b7d8dc3231a318a5c8b5325fba3478c6acd13
niinaIsHere <niina.kampman@gmail.com>
2026-08-08 23:30:58 +0300

    Ch5 ex 21

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
   const [weather, setWeather] = useState("");
   const [visibility, setVisibility] = useState("");
   const [date, setDate] = useState("");
+  const [errormessage, setErrormessage] = useState("");
 
   useEffect(() => {
     axios.get<Diary[]>("http://localhost:3000/api/diaries").then((response) => {
@@ -15,7 +16,7 @@ const App = () => {
     });
   }, []);
 
-  const onSubmit = (event: React.SyntheticEvent) => {
+  const onSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
 
     const newDiary = {
@@ -24,31 +25,49 @@ const App = () => {
       visibility: visibility,
     };
 
-    axios
-      .post("http://localhost:3000/api/diaries", newDiary)
-      .then((response) => {
-        setDiaries(diaries.concat(response.data));
-        setWeather("");
-        setVisibility("");
-        setDate("");
-      });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/diaries",
+        newDiary,
+      );
+      setDiaries(diaries.concat(response.data));
+      setWeather("");
+      setVisibility("");
+      setDate("");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setErrormessage(error.response?.data.error[0].message);
+        console.log(error.response?.data.error[0].message);
+        console.error(error.response);
+      } else {
+        console.error(error);
+      }
+    }
   };
 
   return (
     <div>
       <h2>Diaries</h2>
 
+      <h3>Add a new diary</h3>
+
+      <p>
+        <em>{errormessage}</em>
+      </p>
+
       <form onSubmit={onSubmit}>
+        date:
         <input value={date} onChange={(event) => setDate(event.target.value)} />
+        weather:
         <input
           value={weather}
           onChange={(event) => setWeather(event.target.value)}
         />
+        visibility:
         <input
           value={visibility}
           onChange={(event) => setVisibility(event.target.value)}
         />
-
         <button type="submit">add</button>
       </form>
 
