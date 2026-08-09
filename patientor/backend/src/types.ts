f230a787688a8347bb826ad3b21d5d867341b8e1
niinaIsHere <niina.kampman@gmail.com>
2026-08-09 20:46:12 +0300

    Ch6 ex 25

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -6,7 +6,52 @@ export type Diagnosis = {
   latin?: string;
 };
 
-export interface Entry {}
+type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+interface HospitalEntry {
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry {
+  employerName: string;
+  sickLeave: SickLeave;
+}
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type Patient = {
   id: string;
